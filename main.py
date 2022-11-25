e03fdd84e9ec5e9850b2818839a6561eeaad228c
JoeyStark007 <84150889+JoeyStark007@users.noreply.github.com>
2022-11-25 13:37:43 -0500

    Send email when event is found

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import smtplib
+import ssl
+import os
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -18,7 +21,20 @@ def extract(source):
     value = extractor.extract(source)['tours']
     return value
 
-def send_email():
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "jgtefera@gmail.com"
+    password = "otbrrkuhxmajvifn"
+
+    receiver = "jgtefera@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print('email was sent')
 
 
@@ -26,9 +42,12 @@ def store(extracted):
     with open("data.txt", "a") as file:
         file.write(extracted + "\n")
 
+
 def read(extracted):
     with open('data.txt', 'r') as file:
         return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
@@ -37,4 +56,4 @@ if __name__ == "__main__":
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(message= "Hey New event was found!")
